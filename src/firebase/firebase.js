cb41389bac53accec410924ee13702e8bd1d9cd3
Malik Kilgore <malik199228@gmail.com>
2020-11-23 20:52:23 -0500

    Added Firebase to project

---
+++ src/firebase/firebase.js
@@ -0,0 +1,31 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+  
+  // Firebase init - project configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBsgTKyKQ6C86ijJeSxcgY_2gMFbQkBfuI",
+    authDomain: "leek-calendar.firebaseapp.com",
+    databaseURL: "https://leek-calendar.firebaseio.com",
+    projectId: "leek-calendar",
+    storageBucket: "leek-calendar.appspot.com",
+    messagingSenderId: "369234813359",
+    appId: "1:369234813359:web:ca969e35ee8677d8780f5a"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+// Utils
+const db = firebase.firestore()
+const auth = firebase.auth()
+
+// Collection references
+const usersCollection = db.collection('users')
+
+export {
+    db,
+    auth,
+    usersCollection
+}
--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuex from 'vuex'
+import './assets/styles.css'
+import { auth } from './firebase/firebase'
 
 createApp(App).use(store).use(router).mount('#app')
